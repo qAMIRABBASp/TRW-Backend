ae22c968a4d1d98ce7116055693ebbd87a23fc70
qAMIRABBASp <aryahymqdm@gmail.com>
2026-02-26 11:53:33 +0330

    Co-authored-by: PGhA22 <PGhA22@users.noreply.github.com>

--- TRW/urls.py
+++ TRW/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from .views import PingView
+from events.views import ConfirmBankTransferView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +30,6 @@ urlpatterns = [
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    path("api/registrations/<int:pk>/confirm-bank-transfer/", ConfirmBankTransferView.as_view()),
 ]
